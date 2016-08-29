bc374a254f7ba2f8bdf0302e75ab4cf6d5cbea20
tatsuya azegami <theorthodoxworks@gmail.com>
2016-08-29 22:36:53 +0900

    Fix SVGが小文字変換されていたため修正（gulp-cheerioのparserOptionsにxmlMode:trueを追加）

--- gulpfile.js
+++ gulpfile.js
@@ -16,43 +16,47 @@ gulp.task('svg', () => {
   gulp.src(paths.svg)
   .pipe($.svgmin())
   .pipe($.svgstore({ inlineSvg: true }))
-  .pipe($.cheerio(($, file) => {
-    // 不要なタグを削除
-    $('style,title,defs').remove();
-    // symbolタグ以外のid属性を削除
-    $('[id]:not(symbol)').removeAttr('id');
-    // Illustratorで付与される「st」と「cls」ではじまるclass属性を削除
-    $('[class^="st"],[class^="cls"]').removeAttr('class');
-    // svgタグ以外のstyle属性を削除
-    $('[style]:not(svg)').removeAttr('style');
-    // data-name属性を削除
-    $('[data-name]').removeAttr('data-name');
-    // fill属性を削除
-    $('[fill]').removeAttr('fill');
-    // svgタグにdisplay:noneを付与（読み込み時、スプライト全体を非表示にするため）
-    $('svg').attr({
-      style: 'display:none'
-    });
+  .pipe($.cheerio({
+    run: ($, file) => {
+      // 不要なタグを削除
+      $('style,title,defs').remove();
+      // symbolタグ以外のid属性を削除
+      $('[id]:not(symbol)').removeAttr('id');
+      // Illustratorで付与される「st」と「cls」ではじまるclass属性を削除
+      $('[class^="st"],[class^="cls"]').removeAttr('class');
+      // svgタグ以外のstyle属性を削除
+      $('[style]:not(svg)').removeAttr('style');
+      // data-name属性を削除
+      $('[data-name]').removeAttr('data-name');
+      // fill属性を削除
+      $('[fill]').removeAttr('fill');
+      // svgタグにdisplay:noneを付与（読み込み時、スプライト全体を非表示にするため）
+      $('svg').attr({
+        style: 'display:none'
+      });
 
-    // _base.htmlに渡すid
-    const symbols = $('svg > symbol').map(function() {
-      return {
-        id: $(this).attr('id')
-      };
-    }).get();
+      // _base.htmlに渡すid
+      const symbols = $('svg > symbol').map(function() {
+        return {
+          id: $(this).attr('id')
+        };
+      }).get();
 
-    // _sample.htmlをルートに生成
-    const temp   = require('gulp-template');
-    const rename = require('gulp-rename');
-    gulp.src(paths.html)
-    .pipe(temp({
-      inlineSvg: $('svg'),
-      symbols: symbols
-    }))
-    .pipe(rename('_sample.html'))
-    .pipe(gulp.dest('./'));
+      // _sample.htmlをルートに生成
+      const temp   = require('gulp-template');
+      const rename = require('gulp-rename');
+      gulp.src(paths.html)
+      .pipe(temp({
+        inlineSvg: $('svg'),
+        symbols: symbols
+      }))
+      .pipe(rename('_sample.html'))
+      .pipe(gulp.dest('./'));
+    },
+    parserOptions: {
+      xmlMode: true
+    }
   }))
-
   .pipe($.rename('sprite.min.svg'))
   .pipe(gulp.dest(dir.dest));
 
